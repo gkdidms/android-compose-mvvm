2b0c43f5b759f70cfb0ea0fe8a7b5673feebe6ce
gkdidms <gkdidms3320@gmail.com>
2022-07-22 00:20:42 +0900

    :lipstick: circularProgressbar animation 적용

--- NewAccountBookProject/app/src/main/java/com/example/newaccountbookproject/view/MainActivity.kt
+++ NewAccountBookProject/app/src/main/java/com/example/newaccountbookproject/view/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.example.newaccountbookproject.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
 import com.example.newaccountbookproject.R
 import com.example.newaccountbookproject.base.BaseActivity
 import com.example.newaccountbookproject.databinding.ActivityMainBinding
@@ -33,30 +30,69 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
                 MainView()
             }
         }
-
-
     }
 
     @Composable
-    private fun MainView(names: List<String> = listOf("ioio", "yeeun")) {
-        Surface(color = Color.Black
+    private fun MainView() {
+        Surface(
+            color = Color.White
         ) {
             Column() {
-                for (name in names) {
-                    TextGreeting(text = name)
-                }
+                MainCircularProgressAnimated()
+                MainButton()
             }
         }
+    }
+
+
+
+    @Composable
+    private fun MainCircularProgressAnimated() {
+        val progressValue = 0.75f
+//        val infiniteTransition = rememberInfiniteTransition()
+        var progress by remember {
+            mutableStateOf(0.0f)
+        }
+
+        //react effect 와 동일함. activity가 실행될때 최초 실행됨.
+        LaunchedEffect(Unit) {
+            progress = 0.7f
+        }
+
+        val progressAnimation by animateFloatAsState(
+            targetValue = progress,
+            animationSpec = tween(1000)
+        )
+        CircularProgressIndicator(
+            progress = progressAnimation,
+            strokeWidth = 5.dp,
+            color = Color.Blue
+        )
+
+        Button(onClick = {
+                         progress = (0 until 100).random().toFloat() / 100
+        },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Blue
+            )) {
+            Text(text = "버튼입니다.", color = Color.White)
+        }
+    }
+
+    @Composable
+    private fun MainButton() {
 
     }
 
     @Composable
     private fun TextGreeting(text: String) {
         Surface(
-            color = Color.White,
             modifier = Modifier.padding(horizontal = 12.dp, vertical = 12.dp)
         ) {
-            Text(text = text, modifier = Modifier.fillMaxWidth())
+            Text(
+                text = text,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 
