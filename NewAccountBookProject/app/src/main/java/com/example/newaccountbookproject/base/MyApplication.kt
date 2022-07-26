8cbe427b4b921b8e789b10086f8c701d1f13aa17
gkdidms <gkdidms3320@gmail.com>
2022-07-27 00:00:25 +0900

    :sparkles: retrofit 추가 + application 전역 추가

--- NewAccountBookProject/app/src/main/java/com/example/newaccountbookproject/base/MyApplication.kt
+++ NewAccountBookProject/app/src/main/java/com/example/newaccountbookproject/base/MyApplication.kt
@@ -1,12 +1,42 @@
 package com.example.newaccountbookproject.base
 
 import android.app.Application
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class MyApplication:Application() {
 
+    val BASE_API = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+
+    //전역변수
+    companion object {
+        lateinit var retrofit: Retrofit
+    }
+
+
     override fun onCreate() {
         super.onCreate()
+        initRetrofitInstance()
+    }
+
+    private fun initRetrofitInstance() {
+        val okHttp = OkHttpClient.Builder()
+            .readTimeout(5000, TimeUnit.SECONDS)
+            .connectTimeout(5000, TimeUnit.SECONDS)
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(BASE_API)
+            .client(okHttp)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
     }
+
+
+
+
 
 }
