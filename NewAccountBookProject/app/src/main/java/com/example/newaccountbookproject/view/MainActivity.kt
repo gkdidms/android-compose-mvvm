fd71f342210fc90043e428ba92f181d99201a7a4
gkdidms <gkdidms3320@gmail.com>
2022-08-04 01:19:17 +0900

    :lipstick: item view 변경

--- NewAccountBookProject/app/src/main/java/com/example/newaccountbookproject/view/MainActivity.kt
+++ NewAccountBookProject/app/src/main/java/com/example/newaccountbookproject/view/MainActivity.kt
@@ -3,21 +3,25 @@ package com.example.newaccountbookproject.view
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.newaccountbookproject.base.BaseActivity
-import com.example.newaccountbookproject.viewmodel.MainViewModel
 import com.example.newaccountbookproject.view.ui.theme.NewAccountBookProjectTheme
+import com.example.newaccountbookproject.viewmodel.MainViewModel
 
 class MainActivity : BaseActivity<MainViewModel>() {
 
@@ -25,6 +29,7 @@ class MainActivity : BaseActivity<MainViewModel>() {
     private val key = "b3a487cd4400b8bb9805b40d9df5ec60"
     private val targetDt = "20220728"
     var num: Int = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,7 +42,7 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
     override fun onStart() {
         super.onStart()
-        viewModel.getMoveList(key=key, targetDt=targetDt)
+        viewModel.getMoveList(key = key, targetDt = targetDt)
     }
 
     @Composable
@@ -48,7 +53,6 @@ class MainActivity : BaseActivity<MainViewModel>() {
             //RecyclerView 같은거임
             LazyColumn() {
                 //MainCircularProgressAnimated()
-
                 items(num) {
                     MainItem(it)
                 }
@@ -58,51 +62,29 @@ class MainActivity : BaseActivity<MainViewModel>() {
 
     @Composable
     private fun MainItem(num: Int) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Text(text = viewModel.dailyBoxOfficeList.value?.get(num)?.rnum ?: "데이터 없음")
-            Text(text = viewModel.dailyBoxOfficeList.value?.get(num)?.movieNm ?: "데이터 없음")
+        Box(modifier = Modifier.padding(10.dp)) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .border(1.dp, Color.Black, RoundedCornerShape(5.dp))
+                    .padding(10.dp, 5.dp)
+            ) {
+                NumText(viewModel.dailyBoxOfficeList.value?.get(num)?.rnum ?: "데이터 없음")
+                TitleText(viewModel.dailyBoxOfficeList.value?.get(num)?.movieNm ?: "데이터 없음")
+            }
         }
     }
 
+    @Composable
+    private fun TitleText(title: String) {
+        Text(text = title, fontSize = 20.sp)
+    }
 
-//    @Composable
-//    private fun MainCircularProgressAnimated() {
-////        val infiniteTransition = rememberInfiniteTransition() 무한대로 동작하는 애니메이션
-//
-//        //데이터 변화를 감지하는 ?? liveData로 어떻게 변형하지 ?
-//        val progress: Float by viewModel.progressNum.observeAsState(0.0f)
-//
-//        //react effect 와 동일함. activity가 실행될때 최초 실행됨.
-////        LaunchedEffect(Unit) {
-////            progress = 0.7f
-////        }
-//
-//        val progressAnimation by animateFloatAsState(
-//            targetValue = progress,
-//            animationSpec = tween(1000)
-//        )
-//
-//        Box() {
-//            CircularProgressIndicator(
-//                progress = progressAnimation,
-//                strokeWidth = 10.dp,
-//                color = Color.Blue,
-//                modifier = Modifier.fillMaxWidth().height(500.dp)
-//            )
-//            Text(
-//                text = "70%",
-//                modifier = Modifier.align(Alignment.Center).padding(vertical = 10.dp),
-//            )
-//        }
-//
-//        Button(
-//            onClick = {},
-//            colors = ButtonDefaults.buttonColors(
-//                backgroundColor = Color.Blue
-//            )) {
-//            Text(text = "버튼입니다.", color = Color.White)
-//        }
-//    }
+    @Composable
+    private fun NumText(num: String) {
+        Text(text = num, fontSize = 18.sp)
+    }
 
     @Preview(showBackground = true, widthDp = 320, heightDp = 640)
     @Composable
